319d3ab33bb7fd755bd567af3c392d113f8bcde0
Dudu <jun.quan.mail@gmail.com>
2018-03-20 22:09:38 +0700

    Add route

--- index.js
+++ index.js
@@ -8,10 +8,12 @@ var ejs = require('ejs');
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 
-var db;
-
 app.listen(3001,'localhost');
 console.log('Listening on 3001');
 app.get('/', (req, res) => {
-  res.render('index.ejs')
+  res.render('index.ejs');
+});
+
+app.get('/:input', (req, res) => {
+  res.render('date.ejs', { date: req.params.input });
 });
