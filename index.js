d67ac61a07bd569f4cdf5d6dd3f616eb31c4ade7
Dudu <jun.quan.mail@gmail.com>
2018-03-20 22:24:48 +0700

    Project complete before refactoring

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const MongoClient = require('mongodb').MongoClient;
-
+const moment = require('moment');
 var ejs = require('ejs');
 
 app.set('view engine', 'ejs');
@@ -15,5 +15,33 @@ app.get('/', (req, res) => {
 });
 
 app.get('/:input', (req, res) => {
+  var date = req.params.input;
+  var unix = null;
+  var natural = null;
+
+  // Check for initial unix time
+  if (+date >= 0) {
+      unix = +date;
+      natural = unixToNat(unix);
+  }
+
+  // Check for initial natural time
+  if (isNaN(+date) && moment(date, "MMMM D, YYYY").isValid()) {
+      unix = +natToUnix(date);
+      natural = unixToNat(unix);
+  }
+
+  var dateObj = { "unix": unix, "natural": natural };
+res.send(dateObj);
   res.render('date.ejs', { date: req.params.input });
 });
+
+function natToUnix(date) {
+  // Conver from natural date to unix timestamp
+  return moment(date, "MMMM D, YYYY").format("X");
+}
+
+function unixToNat(unix) {
+  // Convert unix timestamp to natural date
+  return moment.unix(unix).format("MMMM D, YYYY");
+}
